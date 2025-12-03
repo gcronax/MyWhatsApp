219059094b21e6ee9c213bd744b50c96a99d92a8
guicamher <guillecampher15@gmail.com>
2025-12-03 16:27:08 +0100

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/mywhatsapp/MainActivity.kt
+++ app/src/main/java/com/example/mywhatsapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.mywhatsapp
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
@@ -14,7 +23,6 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material.icons.Icons
 
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +31,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -32,12 +41,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mywhatsapp.ui.theme.MyWhatsAppTheme
+import com.example.mywhatsapp.ui.theme.Pink40
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -47,85 +58,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyWhatsAppTheme {
 
-                var tituloPasado by rememberSaveable { mutableStateOf("IES caminas") }
                 var show by remember { mutableStateOf(false) }
                 var select by remember { mutableIntStateOf(1) }
 
                 val navController = rememberNavController()
+                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+                    rememberTopAppBarState()
+                )
+                var state by remember { mutableStateOf(0) }
+                val titles = listOf("Chat", "Novedades", "Llamadas")
 
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     topBar = {
                         CenterAlignedTopAppBar(
+                            scrollBehavior = scrollBehavior,
                             title = { Text(text = "MyWhatsApp")
                             },
                             actions = {
-                                var expanded by remember { mutableStateOf(false) }
-
-                                Column(Modifier.padding(20.dp)) {
-                                    IconButton(
-                                        onClick = {
-                                            expanded = true
-                                        }) {
-                                        Icon(imageVector = Icons.Filled.MoreVert,
-                                            contentDescription = "Buscar",
-                                        )
-                                    }
-
-                                    DropdownMenu(
-                                        expanded = expanded,
-                                        onDismissRequest = { expanded = false },
-                                    ) {
-                                        DropdownMenuItem(
-                                            onClick = { expanded = false
-                                                select=1
-                                            },
-                                            leadingIcon ={
-                                                Icon(imageVector = Icons.Filled.Share,
-                                                    contentDescription = "stagg")
-                                            },
-                                            text = {Text(text = "a"
-                                            )}
-//                                                    stringResource(R.string.stag123)
-
-                                        )
-                                        DropdownMenuItem(
-                                            onClick = { expanded = false
-                                                select=2
-                                            },
-                                            leadingIcon ={
-                                                Icon(imageVector = Icons.Filled.Lock,
-                                                    contentDescription = "lazy")
-                                            },
-                                            text = {Text(text = "lazy")},
-
-                                            )
-
-                                    }
+                                IconButton(
+                                    onClick = {
+                                    }) {
+                                    Icon(imageVector = Icons.Filled.Search,
+                                        contentDescription = "Buscar",
+                                    )
+                                }
+                                IconButton(
+                                    onClick = {
+                                    }) {
+                                    Icon(imageVector = Icons.Filled.Share,
+                                        contentDescription = "Buscar",
+                                    )
                                 }
                             },
                             colors= TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primary,
-                                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                                containerColor =  Color(0xFF007B70),
+                                titleContentColor = Color(0xFFBCC2B1),
                             )
                         )
-                        var state by remember { mutableStateOf(0) }
-                        val titles = listOf("Tab 1", "Tab 2", "Tab 3 with lots of text")
-                        Column {
-                            PrimaryTabRow(selectedTabIndex = state) {
-                                titles.forEachIndexed { index, title ->
-                                    Tab(
-                                        selected = state == index,
-                                        onClick = { state = index },
-                                        text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) },
-                                    )
-                                }
-                            }
-                            Text(
-                                modifier = Modifier.align(Alignment.CenterHorizontally),
-                                text = "Text tab ${state + 1} selected",
-                                style = MaterialTheme.typography.bodyLarge,
-                            )
-                        }
                     },
                     floatingActionButton = {
 
@@ -141,16 +110,41 @@ class MainActivity : ComponentActivity() {
                     }
                 )
                 { innerPadding ->
-                    when(select){
-                        1->principal(modifier = Modifier.padding(innerPadding))
-                        2->secundario(modifier = Modifier.padding(innerPadding))
-                    }
-
-                    NavHost(navController = navController, startDestination = "Principal") {
-                        composable("Principal") {
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        PrimaryTabRow(
+                            selectedTabIndex = state,
+                            containerColor =  Color(0xFF007B70),
+                            contentColor = Color(0xFFBCC2B1)
+                            ) {
+                            titles.forEachIndexed { index, title ->
+                                Tab(
+                                    selected = state == index,
+                                    onClick = { state = index },
+                                    text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) },
+                                )
+                            }
 
                         }
-                        composable("Secundario") {
+                        when(state){
+                            0->Chat(modifier = Modifier)
+                            1->Novedades(modifier = Modifier)
+                            2->Llamadas(modifier = Modifier)
+
+                        }
+
+                    }
+
+
+
+
+                    NavHost(navController = navController, startDestination = "Chat") {
+                        composable("Chat") {
+
+                        }
+                        composable("Novedades") {
+
+                        }
+                        composable("Llamadas") {
 
                         }
 
@@ -165,12 +159,20 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun principal(modifier: Modifier) {
+fun Chat(modifier: Modifier) {
+    Row (modifier=modifier){ Text(text = "chat") }
 
 }
 
 @Composable
-fun secundario(modifier: Modifier) {
+fun Novedades(modifier: Modifier) {
+    Text(text = "novedades")
+
+}
+
+@Composable
+fun Llamadas(modifier: Modifier) {
+    Text(text = "llamadas")
 
 }
 
